8430950c161322b37b67356389c9b56824dc4814
Nicolas DUBIEN <github@dubien.org>
2020-10-26 14:07:13 +0100

    Add support for typings

---
+++ copyTypings.js
@@ -0,0 +1,31 @@
+const fs = require("fs");
+const path = require("path");
+const { directories } = require("./conf");
+
+const distDirectoryPath = path.join(__dirname, "dist");
+for (const directoryName of directories) {
+  console.log(
+    `Copying typings into dist/ for "@codemirror/next/${directoryName}"`
+  );
+  const directoryPath = path.join(distDirectoryPath, directoryName);
+  const nodeModulesPath = path.join(
+    __dirname,
+    "node_modules",
+    "@codemirror",
+    "next",
+    directoryName,
+    "dist"
+  );
+
+  const files = fs.readdirSync(nodeModulesPath);
+  files.forEach((fileName) => {
+    if (!fileName.endsWith(".d.ts")) {
+      return;
+    }
+    console.log(`> ${fileName}`);
+    fs.writeFileSync(
+      path.join(directoryPath, fileName),
+      fs.readFileSync(path.join(nodeModulesPath, fileName))
+    );
+  });
+}
